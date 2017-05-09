4e11608dceb8dd34be0f42ebf0d41f9a9b6c6f3a
Russ <russ.king@evestor.co.uk>
2017-05-10 00:46:31 +0100

    tut 12 - enemies kill player

--- Assets/Scripts/SlimeController.cs
+++ Assets/Scripts/SlimeController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class SlimeController : MonoBehaviour {
@@ -6,18 +7,23 @@ public class SlimeController : MonoBehaviour {
 	public float moveSpeed;
 	public float timeBetweenMove;
 	public float timeToMove;
+	public float waitToReload;
 
 	private Rigidbody2D myRigidBody;
 	private bool isMoving;
 	private Vector3 moveDirection;
+	private bool isReloading;
+	private GameObject thePlayer;
 
 	private float timeBetweenMoveCounter;
 	private float timeToMoveCounter;
 
 	// Use this for initialization
 	void Start () {
-		timeBetweenMoveCounter = timeBetweenMove;
-		timeToMoveCounter = timeToMove;
+		
+		timeBetweenMoveCounter = Random.Range(timeBetweenMove * .75f, timeBetweenMove * 1.25f);
+		timeToMoveCounter = Random.Range(timeToMove * .75f, timeToMove * 1.25f);
+
 		myRigidBody = GetComponent<Rigidbody2D> ();
 	}
 	
@@ -29,20 +35,36 @@ public class SlimeController : MonoBehaviour {
 
 			if (timeToMoveCounter < 0) {
 				isMoving = false;
-				timeBetweenMoveCounter = timeBetweenMove;
+				timeBetweenMoveCounter = Random.Range(timeBetweenMove * .75f, timeBetweenMove * 1.25f);
 			}
-
-			Debug.Log (myRigidBody.velocity);
 
 		} else {
 			timeBetweenMoveCounter -= Time.deltaTime;
 			myRigidBody.velocity = Vector2.zero;
 			if (timeBetweenMoveCounter < 0) {
 				isMoving = true;
-				timeToMoveCounter = timeToMove;
+				timeToMoveCounter = Random.Range(timeToMove * .75f, timeToMove * 1.25f);
 
 				moveDirection = new Vector3 (Random.Range (-1f, 1f) * moveSpeed, Random.Range (-1f, 1f) * moveSpeed, 0);
 			}
+		}
+
+		if (isReloading) {
+			waitToReload -= Time.deltaTime;
+			if (waitToReload < 0) {
+				int scene = SceneManager.GetActiveScene ().buildIndex;
+				SceneManager.LoadScene (scene);
+				thePlayer.SetActive (true);
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.name == "Player") {
+			thePlayer = other.gameObject;
+			other.gameObject.SetActive (false);
+			isReloading = true;
+
 		}
 	}
 }
